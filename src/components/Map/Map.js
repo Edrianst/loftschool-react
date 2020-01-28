8b5acbd3828b57ac57aefc3f54ec540aaaaf0116
Denis Kirsanov <excite90@mail.ru>
2020-01-28 15:49:12 +0300

    Сделал роутинг по страницам

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { mapboxToken as token } from '../../constants/Tokens';
 
 const Map = () => {
     let mapContainer;
     useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZWRyaWFuc3QiLCJhIjoiY2s1cXhtOGIwMDJoMzNrcHJiZnNoZjgwbiJ9.bPXuNWmgVQ7mURGhSJdFow';
+        mapboxgl.accessToken = token;
         const map = new mapboxgl.Map({
             container: mapContainer,
             style: 'mapbox://styles/mapbox/light-v10',
@@ -16,14 +17,7 @@ const Map = () => {
         }
     }, [mapContainer]);
 
-    const style = {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        width: '100%',
-        zIndex: 0
-    };
-    return <div className="map" style={style} ref={el => mapContainer = el} />;
+    return <div className="map" ref={el => mapContainer = el} />;
 };
 
 export default Map;
