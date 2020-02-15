feda972f686a3b5e859f77f33c74acb063738163
Denis Kirsanov <excite90@mail.ru>
2020-02-15 21:01:45 +0300

    добавил пару тестов для саг

---
+++ src/modules/Auth/__test__/saga.test.js
@@ -0,0 +1,42 @@
+import {authorizationSaga} from "../sagas";
+import {put, call} from "redux-saga/effects";
+import {fetchAuthSuccess, fetchAuthFailure} from "../actions";
+import {authRequest} from "../api";
+
+describe('authorization saga', () => {
+    it('should fail if action payload is incorrect', () => {
+        const action = {
+            payload: null
+        };
+
+        const response = {
+            success: false,
+            error: 'error'
+        };
+        const gen = authorizationSaga(action);
+
+        expect(gen.next(false).value).toEqual(call(authRequest, action.payload));
+        expect(gen.next(response).value).toEqual(put(fetchAuthFailure(response)));
+        expect(gen.next().done).toBeTruthy();
+    });
+
+    it('should call success action if payload is correct', () => {
+        const action = {
+            payload: {
+                email: 'test',
+                password: 'test'
+            }
+        };
+
+        const response = {
+            success: true,
+            token: 'some token'
+        };
+
+        const gen = authorizationSaga(action);
+
+        expect(gen.next(true).value).toEqual(call(authRequest, action.payload));
+        expect(gen.next(response).value).toEqual(put(fetchAuthSuccess(response)));
+        expect(gen.next().done).toBeTruthy();
+    })
+});
