3c8b6a128f300d1059a0c41d1b5f01912ccb3e79
Denis Kirsanov <excite90@mail.ru>
2020-02-16 00:17:34 +0300

    дописал тесты для саг

--- src/modules/Map/__test__/sagas.test.js
+++ src/modules/Map/__test__/sagas.test.js
@@ -1,7 +1,7 @@
-import {addressListSaga} from "../sagas";
+import {addressListSaga, routeSaga} from "../sagas";
 import {call, put} from 'redux-saga/effects';
-import {addressRequest} from "../api";
-import {fetchAddressSuccess} from "../actions";
+import {addressRequest, routesRequest} from "../api";
+import {fetchAddressSuccess, fetchRouteSuccess} from "../actions";
 
 describe('addressListSaga', () => {
 
@@ -15,4 +15,19 @@ describe('addressListSaga', () => {
         expect(gen.next(response).value).toEqual(put(fetchAddressSuccess(response.addresses)));
         expect(gen.next().done).toBeTruthy();
     });
+});
+
+describe('route saga', () => {
+    it('should get coordinates for route', () => {
+        let action = {
+            address1: 'address1',
+            address2: 'address2'
+        };
+        const gen = routeSaga(action);
+
+        const response = [[10, 10], [20, 20]];
+
+        expect(gen.next().value).toEqual(call(routesRequest, action.payload));
+        expect(gen.next(response).value).toEqual(put(fetchRouteSuccess({status: true, coordinates: response})))
+    })
 });
